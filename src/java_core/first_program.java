0929ecbfd857bec20b55d5296277626406880ea0
Jobaer Hossain <hsjobaer.lis.edu@outlook.com>
2023-03-19 21:13:41 +0600

    writing first program with java

---
+++ src/java_core/first_program.java
@@ -0,0 +1,9 @@
+package java_core;
+
+public class first_program {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("I am Jobaer Hossain.");
+        System.out.println("Welcome to the world of java technologies...!");
+    }
+}
