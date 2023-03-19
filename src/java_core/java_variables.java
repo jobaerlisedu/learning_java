1ae0d1385c986a6fff03e09e6c2cbf45cc1c4636
Jobaer Hossain <hsjobaer.lis.edu@outlook.com>
2023-03-19 21:15:53 +0600

    using variables to store data in java

---
+++ src/java_core/java_variables.java
@@ -0,0 +1,11 @@
+package java_core;
+
+public class java_variables {
+    public static void main(String[] args) {
+        String name = "Jobaer Hossain";
+        int age = 23;
+
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+    }
+}
