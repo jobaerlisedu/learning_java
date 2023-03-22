a209795d3fe0f1759ed76fa38182e95aaec8b251
Jobaer Hossain <hsjobaer.lis.edu@outlook.com>
2023-03-22 12:29:45 +0600

    write a program to calculate the factorial in java

--- src/java_core/java_assignment_factorial_number.java
+++ src/java_core/java_assignment_factorial_number.java
@@ -2,7 +2,7 @@ package java_core;
 
 import java.util.Scanner;
 
-public class java_exercise_factorial_number {
+public class java_assignment_factorial_number {
 
     public static void main(String[] args) {
 
