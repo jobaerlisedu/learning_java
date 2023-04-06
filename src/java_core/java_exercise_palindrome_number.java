f4d63d7d7ba262941be1af2cf1db71115ce6c329
Jobaer Hossain <hsjobaer.lis.edu@outlook.com>
2023-04-06 10:25:54 +0600

    writing a program to check palindrome numbers

--- src/java_core/java_exercise_palindrome_number.java
+++ src/java_core/java_exercise_palindrome_number.java
@@ -8,7 +8,29 @@ public class java_exercise_palindrome_number {
 
         Scanner userInput = new Scanner(System.in);
 
-        int number, temp, r;
+        int number, temp, r, sum = 0;
+
+        System.out.print("Enter a positive number : ");
+        number = userInput.nextInt();
+        temp = number;
+
+        while (temp != 0) {
+
+            r = temp % 10;
+            sum = sum * 10 + r;
+            temp = temp / 10;
+
+        }
+
+        if (sum == number) {
+
+            System.out.println(number + " is a palindrome number.");
+
+        } else {
+
+            System.out.println(number + " is not a palindrome number.");
+
+        }
 
     }
 
